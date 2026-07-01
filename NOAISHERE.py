02ed77460decced2541d2f1f9092212ceaad9c14
Noa <50514808+nokiki6@users.noreply.github.com>
2026-07-01 16:33:35 +1200

    hi
    changes

--- NOAISHERE.py
+++ NOAISHERE.py
@@ -1,1 +1,3 @@
 hello guys this is a  dont forget im n here not lost 
+while True:
+    print("im real")
--- hehe.py
+++ hehe.py
@@ -2,3 +2,6 @@ while True:
     print("noa is lost")
     print("hello world")
 
+
+while True:
+    print("im real")
