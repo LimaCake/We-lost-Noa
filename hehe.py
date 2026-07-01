7f9cf44ef75d36eaf317c32ca6eb48144b5d4b0d
Noa <50514808+nokiki6@users.noreply.github.com>
2026-07-01 16:38:57 +1200

    hi2
    helloooo

--- hehe.py
+++ hehe.py
@@ -28,3 +28,29 @@ elif hello == 13:
     print("you picked 13")
 elif hello == 14:
     print("you picked 14")
+elif hello == 15:
+    print("you picked 2")
+elif hello == 16:
+    print("you picked 3")
+elif hello == 17:
+    print("you picked 4")
+elif hello == 5:
+    print("you picked 5")
+elif hello == 6:
+    print("you picked 6")
+elif hello == 7:
+    print("you picked 7")
+elif hello == 8:
+    print("you picked 8")
+elif hello == 9:
+    print("you picked 8")
+elif hello == 10:
+    print("you picked 9")
+elif hello == 11:
+    print("you picked 10")
+elif hello == 12:
+    print("you picked 12")
+elif hello == 13:
+    print("you picked 13")
+elif hello == 14:
+    print("you picked 14")
